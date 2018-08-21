05987eedc74ead996a77c92f4fcb60ff0205b662
Alex Reich <atrain.reich@gmail.com>
2018-08-20 23:07:21 -0600

    Set up server side for hosting multiple players at once

--- server.js
+++ server.js
@@ -15,10 +15,20 @@ http.listen(app.get('port'), function(){
 });
 
 // Tell Socket.io to start accepting connections
+// create dict to keep all players as key/value pairs
+var players = {};
 io.on('connection', function(socket){
   console.log("New client has connected with id:",socket.id);
   socket.on('new-player',function(state_data){ // Listen for new-player event on this client 
     console.log("New player has state:",state_data);
-    socket.broadcast.emit('create-player',state_data);
+    // add new player to dict
+    players[socket.id] = state_data;
+    // send an update event
+    io.emit('update-players',players);
+  })
+  socket.on('disconnect', function(){
+    // delete player from dict on disconnect
+    delete players[socket.id];
+    // send an update event
   })
 })
